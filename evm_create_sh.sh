28b5826adf590e1edde42140d13da36d567d9558
Kosuke Kataoka <38752733+kataksk@users.noreply.github.com>
2022-04-02 23:36:17 +0900

    Update evm_create_sh.sh

--- evm_create_sh.sh
+++ evm_create_sh.sh
@@ -1,9 +1,9 @@
-job_file_temp='./pbs_job_comment.txt'
-execute_EVM_commands_path=$1
+job_file_temp=$1
+execute_EVM_commands_path=$2
 mkdir ../execute_EVM_commands
-python evm_create_sh.py $1
+python evm_create_sh.py $2
 for pathfile in `ls ../execute_EVM_commands/*`; do
-    cat $job_file_temp $pathfile > ../execute_EVM_commands/$pathfile'.sh'
+    cat $1 $pathfile > ../execute_EVM_commands/$pathfile'.sh'
 done
 find ../execute_EVM_commands -type f | grep -v -E '*.sh' | xargs rm -rf
 sh_file=(`find ../execute_EVM_commands -type f`)
